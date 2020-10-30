c3cdcc27cde352db3131ff387103a74e658924d8
mikeevmm <miguelmurca@gmail.com>
2020-10-30 13:17:42 +0000

    feat: first commit

---
+++ src/main.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include "include/maybe.hh"
+
+maybe::Maybe<int> find_in_array(int* arr, size_t size, int element) {
+  for (int i = 0; i < size; ++i) {
+    if (arr[i] == element) return maybe::some(arr[i]);
+  }
+  return maybe::none<int>();
+}
+
+int main(void) {
+  int array[] = {1, 2, 5, 6};
+  int to_find[] = {3, 6};
+  for (int i = 0; i < 2; ++i) {
+    // You can do it the functional way...
+    find_in_array(array, 4, to_find[i])
+        .map<int>([](int x) { return x * x; })
+        .then([](int x) { printf("Squared is %i\n", x); });
+
+    // Or the imperative way
+    auto found = find_in_array(array, 4, to_find[i]);
+    if (found.is_some()) {
+      auto squared = found.unwrap();
+      printf("Cubed is %i\n", squared * squared * squared);
+    }
+  }
+  return 0;
+}
